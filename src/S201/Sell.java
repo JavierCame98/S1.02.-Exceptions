04750f4240342eb51c215de854806181ed162257
Javier Castillo <javiercame98@gmail.com>
2025-11-27 10:06:34 +0100

    feat: creating Sell class with a list of products and adding method and total price calculation method with empty sale exception message.

--- src/S201/Sell.java
+++ src/S201/Sell.java
@@ -1,4 +1,30 @@
 package S201;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sell {
+    private List<Product> products;
+    private double totalPriceOfSell;
+
+    public Sell(){
+        this.products = new ArrayList<>();
+    }
+
+    public void addProduct(Product p){
+        products.add(p);
+    }
+
+    public double totalPriceCalculation() throws EmptySaleException{
+        if(products.isEmpty()){
+            throw new EmptySaleException("For a sale first you have to make products");
+        }
+        double totalPrice = 0;
+        for(Product p : products){
+            totalPrice += p.getPrice();
+        }
+        totalPriceOfSell = totalPrice;
+        return totalPriceOfSell;
+    }
+
 }
