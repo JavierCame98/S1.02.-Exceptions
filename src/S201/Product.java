11211f644d264a72bc18eff4ba049b029bfb75b2
Javier Castillo <javiercame98@gmail.com>
2025-11-27 10:02:03 +0100

    feat: creating Product class with its attributes, constructor and getter.

--- src/S201/Product.java
+++ src/S201/Product.java
@@ -1,4 +1,16 @@
 package S201;
 
 public class Product {
+
+    private String name;
+    private int price;
+
+    public Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
