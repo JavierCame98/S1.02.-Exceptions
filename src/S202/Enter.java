4979594a7008fb582b71b7cbd76e2c89c78aa48f
Javier Castillo <javiercame98@gmail.com>
2025-11-27 11:21:07 +0100

    feat: creating lector byte. Trying to catch a InputMismatchException.

--- src/S202/Enter.java
+++ src/S202/Enter.java
@@ -7,25 +7,25 @@ public class Enter {
 
     static Scanner userEnter = new Scanner(System.in);
 
-    public static byte llegirByte(String missatge){
+    public static byte lectorByte(String missatge){
         boolean valid = false;
-        byte value = 0;
-        System.out.println("Enter a single byte value (-128 to 127)");
+        byte userValue = 0;
 
         while(!valid){
             System.out.println(missatge);
             try{
-                byte userValue = userEnter.nextByte();
-                if(userValue >= -128 || userValue <= 127){
-                    valid = true;
-                }
-            }catch(InputMismatchException e){
-                System.out.println("Please introduce a valid input");
-            }
+                userValue = userEnter.nextByte();
+                valid = true;
 
+            }catch(InputMismatchException e){
+                System.out.println("Please introduce a valid input (-128 to 127)");
+                userEnter.next();
+            }
         }
-       return value;
+       return userValue;
     }
+
+
 
 
 }
