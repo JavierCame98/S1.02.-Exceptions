899edc905232b5089234a0586eeeae98276046ed
Javier Castillo <javiercame98@gmail.com>
2025-11-27 11:21:37 +0100

    feat: testing lectorByte method

--- src/S202/Main.java
+++ src/S202/Main.java
@@ -1,4 +1,19 @@
 package S202;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        String messageSelectionByte = "Introduce a byte number: ";
+        byte userSelection = Enter.lectorByte(messageSelectionByte);
+        System.out.println("The user byte is: " + userSelection);
+
+
+
+
+
+
+    }
 }
