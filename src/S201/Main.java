8f54bff195ad8f225f4eb1e09ca4683e4f716d40
Javier Castillo <javiercame98@gmail.com>
2025-11-27 10:09:46 +0100

    feat: Creating the main class to catch the exception with try-catch, to save the calculate total price method and display it in the system, and the test to catch the IndexOutOfBonusException.

--- src/S201/Main.java
+++ src/S201/Main.java
@@ -1,4 +1,43 @@
 package S201;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("1.Trying to catch EmptySaleException for an empty sell");
+        Sell firstSell = new Sell();
+
+        try{
+            firstSell.totalPriceCalculation();
+        }catch (EmptySaleException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("2.Saving and showing totalPriceCalculation method");
+
+        Product firstProduct = new Product("Phone",800);
+        Product secondProduct = new Product("Television",1000);
+
+        firstSell.addProduct(firstProduct);
+        firstSell.addProduct(secondProduct);
+
+        try {
+            System.out.println("The total price of the selling is " + firstSell.totalPriceCalculation());
+        }catch (EmptySaleException e){
+            System.out.println("Unexpected error");
+        }
+
+        System.out.println("3.Trying to catch IndexOutOfBoundsException in a Products List");
+
+        try{
+            List<Product> productsList = new ArrayList<>();
+            productsList.add(firstProduct);
+            productsList.get(2);
+        }catch(IndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
